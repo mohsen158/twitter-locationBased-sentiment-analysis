7ae982678fd33db3911ad799deadb00c8905c3b1
mohsen <rahnamaei_158"yahoo.com>
2020-06-21 17:26:12 +0430

    change server index file to dist folder

--- server/index.js
+++ server/index.js
@@ -13,14 +13,17 @@ const router = express.Router();
 
 const app = express();
 const server = require("http").Server(app);
+path = require("path");
 
 const io = require("socket.io")(server);
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(path.join(__dirname + "/../dist")));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname + "/../dist/index.html"))
+);
 
 server.listen(port);
 app.use(function(req, res, next) {
